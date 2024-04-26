ec9e1aaa774a28ed630ee436db8e71192329b9a3
Jordan Songer <peer@peer.id.au>
2024-04-27 03:44:31 +0930

    flip some defaults for mariaDB

--- pkg/database/mysql/dump.go
+++ pkg/database/mysql/dump.go
@@ -89,7 +89,7 @@ const headerTmpl = `-- Go SQL Dump {{ .DumpVersion }}
 `
 
 const createUseDatabaseHeader = `
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ ` + "`{{.Database}}`" + ` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ ` + "`{{.Database}}`" + ` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE uca1400_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE ` + "`{{.Database}}`;"
 
